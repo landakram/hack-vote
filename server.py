a55f383c2cc5298bdbca17b7b4296716d4ad5fba
Jesse Pollak <jpollak92@gmail.com>
2012-04-04 21:21:50 -0700

    Everything working except number
    bad fix

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, request, redirect, g, render_template, jsonify
 import twilio.twiml
 import logging
-from twilio.rest import TwilioRestClient
 
 
 
@@ -25,22 +24,22 @@ def list():
 @app.route('/vote', methods=['POST'])
 def vote():
     from_number = request.args.get('From', None)
-    client = TwilioRestClient(os.environ['ACCOUNT_SID'], os.environ['AUTH_TOKEN'])
-    app.logger.debug("Number is " + from_number)
+    app.logger.debug("Number is %s" % from_number)
     # number exists
+    resp = twilio.twiml.Response()
     if from_number in numbers:
-        message = client.sms.messages.create(to=from_number,from_="+14156589963",body="Thanks, but you already voted!")
+        resp.sms("Thanks, but you already voted!")
     else:
         body = request.args.get('Body', '')
         letters = "ABCDEFGHIJKLMNOP"
         ident = letters.find(body.strip())
         if ident == -1 or ident >= len(projects):
-            message = client.sms.messages.create(to=from_number,from_="+14156589963",body="That is an invalid vote, please try again!")
+            resp.sms('That is an invalid vote, please try again!')
         else:
             projects[ident]['votes'] += 1
             numbers.add(from_number)
-            message = client.sms.messages.create(to=from_number,from_="+14156589963",body="Thank you for your vote!")
-    return 'Thank you!'
+            resp.sms('Thank you for your vote!')
+    return str(resp)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
